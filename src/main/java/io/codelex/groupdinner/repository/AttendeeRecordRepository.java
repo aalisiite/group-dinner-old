3f919d41223517005e945e7e6ce94aac3f1aef7e
byChriss <krists2013>
2019-04-05 14:40:42 +0300

    added-queries

--- src/main/java/io/codelex/groupdinner/repository/AttendeeRecordRepository.java
+++ src/main/java/io/codelex/groupdinner/repository/AttendeeRecordRepository.java
@@ -2,7 +2,17 @@ package io.codelex.groupdinner.repository;
 
 import io.codelex.groupdinner.repository.model.AttendeeRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AttendeeRecordRepository extends JpaRepository<AttendeeRecord, Long> {
-    
+
+
+    @Query("select attendee from AttendeeRecord attendee"
+            + " where attendee.dinner = :id "
+            + " and attendee.status = :status ")
+    List<AttendeeRecord> findDinnerAttendees(@Param("dinner") Long id,
+                                             @Param("status") boolean status);
 }
