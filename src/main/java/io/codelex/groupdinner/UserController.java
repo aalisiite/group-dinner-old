b0a246551f614f74e9b6b219c834fe23db701706
byChriss <krists2013>
2019-04-05 16:20:32 +0300

    authorization

--- src/main/java/io/codelex/groupdinner/UserController.java
+++ src/main/java/io/codelex/groupdinner/UserController.java
@@ -1,21 +1,17 @@
 package io.codelex.groupdinner;
 
-import io.codelex.groupdinner.InMemory.InMemoryUserModule;
-import io.codelex.groupdinner.repository.AttendeeRecordRepository;
-import io.codelex.groupdinner.repository.model.AttendeeRecord;
+
 import io.codelex.groupdinner.api.CreateDinnerRequest;
 import io.codelex.groupdinner.repository.model.DinnerRecord;
 import io.codelex.groupdinner.api.JoinDinnerRequest;
-import io.codelex.groupdinner.InMemory.service.AttendeeService;
-import io.codelex.groupdinner.InMemory.service.DinnerService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api")
