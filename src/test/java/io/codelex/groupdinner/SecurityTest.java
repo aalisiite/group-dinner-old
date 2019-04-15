24c3eb0eaff15564b7f5fcfb919a87adedc849e4
alise <alka.alise@gmail.com>
2019-04-15 17:54:53 +0300

    test

--- src/test/java/io/codelex/groupdinner/SecurityTest.java
+++ src/test/java/io/codelex/groupdinner/SecurityTest.java
@@ -5,11 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpEntity.EMPTY;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.util.UriComponentsBuilder.fromPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -25,6 +33,57 @@ public class SecurityTest {
     public void customer_account_should_be_secured_by_default() {
         var result = restTemplate.getForEntity("/api/account", String.class);
         assertEquals(FORBIDDEN, result.getStatusCode());
+    }
+
+    @Test
+    public void customer_should_be_authorised_on_registration() {
+        var result = register();
+        assertEquals(OK, result.getStatusCode());
+
+        var sessionId = sessionId(result);
+        assertEquals(OK, accessAccount(sessionId).getStatusCode());
+    }
+
+    private ResponseEntity<Void> register() {
+        var uri = fromPath("/api/register")
+                .queryParam("email", email)
+                .queryParam("password", password)
+                .build()
+                .toUri();
+
+        return restTemplate.postForEntity(uri, EMPTY, Void.class);
+    }
+
+    private ResponseEntity<Void> signIn() {
+        var uri = fromPath("/api/sign-in")
+                .queryParam("email", email)
+                .queryParam("password", password)
+                .build()
+                .toUri();
+
+        return restTemplate.postForEntity(uri, EMPTY, Void.class);
+    }
+
+    private ResponseEntity<Void> signOut(String sessionId) {
+        return restTemplate.exchange("/api/sign-out", POST, request(sessionId), Void.class);
+    }
+
+    private ResponseEntity<String> accessAccount(String sessionId) {
+        return restTemplate.exchange("/api/account", GET, request(sessionId), String.class);
+    }
+
+    private ResponseEntity<String> accessAdminAccount(String sessionId) {
+        return restTemplate.exchange("/admin-api/account", GET, request(sessionId), String.class);
+    }
+
+    private String sessionId(ResponseEntity<?> result) {
+        return result.getHeaders().getFirst("Set-Cookie");
+    }
+
+    private HttpEntity<?> request(String sessionId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Cookie", sessionId);
+        return new HttpEntity<>(headers);
     }
     
 }
